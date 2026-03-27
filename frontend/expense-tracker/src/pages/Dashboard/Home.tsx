315cf232e2905d38703997ea98c22851f1d4e6ae
吴伊玲 <woo12310@163.com>
2026-03-27 16:35:53 +0800

    feat(dashboard): 实现仪表盘数据获取和展示功能

--- frontend/expense-tracker/src/pages/Dashboard/Home.tsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.tsx
@@ -1,5 +1,137 @@
+import { dashboardApi } from "../../api/dashboard";
+import { useEffect, useState } from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import { useNavigate } from "react-router-dom";
+import InfoCard from "../../components/Cards/InfoCard";
+
+import { IoMdCard } from "react-icons/io";
+import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+import { addThounsandsSeparate } from "../../utils/helper";
+import RecentTransaction from "../../components/Dashboard/RecentTransaction";
+import FinanceOverview from "../../components/Dashboard/FinanceOverview";
+import ExpenseTransactions from "../../components/Dashboard/ExpenseTransactions";
+import Last30DaysExpenses from "../../components/Dashboard/Last30DaysExpenses";
+import RecentIncomeWithChart from "../../components/Dashboard/RecentIncomeWithChart";
+import RecentIncome from "../../components/Dashboard/RecentIncome";
+
+export interface Transaction {
+  _id: string;
+  amount: number;
+  category?: string;
+  source?: string;
+  icon: string;
+  date: string;
+}
+interface DashboardData {
+  totalBalance: number;
+  totalIncome: number;
+  totalExpense: number;
+  last30DaysExpenses: {
+    total: number;
+    transactions: Transaction[];
+  };
+  last60DaysIncomes: {
+    total: number;
+    transactions: Transaction[];
+  };
+  recentTransactions: {
+    _id: string;
+    amount: number;
+    type: "income" | "expense";
+    category?: string;
+    source?: string;
+    icon: string;
+    date: string;
+  }[];
+}
+
 const Home = () => {
-  return <div>Home</div>;
+  useUserAuth();
+
+  const navigate = useNavigate();
+  const [dashboardData, setDashboardData] = useState<DashboardData | null>(
+    null,
+  );
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchDashboardData = async () => {
+      setLoading(true);
+      try {
+        const res = await dashboardApi.getDashboardData();
+        if (res.data) {
+          setDashboardData(res.data);
+        }
+      } catch (error) {
+        console.error("获取仪表盘数据失败：", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDashboardData();
+    return () => {};
+  }, []);
+  return (
+    <DashboardLayout activeMenu="dashboard">
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Balance"
+            value={addThounsandsSeparate(dashboardData?.totalBalance || 0)}
+            color="bg-primary"
+          />
+          <InfoCard
+            icon={<LuWalletMinimal />}
+            label="Total Income"
+            value={addThounsandsSeparate(dashboardData?.totalIncome || 0)}
+            color="bg-orange-500"
+          />
+          <InfoCard
+            icon={<LuHandCoins />}
+            label="Total Expense"
+            value={addThounsandsSeparate(dashboardData?.totalExpense || 0)}
+            color="bg-red-500"
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+          <RecentTransaction
+            transactions={dashboardData?.recentTransactions}
+            onSeeMore={() => navigate("/expense")}
+          />
+
+          <FinanceOverview
+            totalBlance={dashboardData?.totalBalance || 0}
+            totalIncome={dashboardData?.totalIncome || 0}
+            totalExpense={dashboardData?.totalExpense || 0}
+          />
+          <ExpenseTransactions
+            transactions={dashboardData?.last30DaysExpenses?.transactions || []}
+            onSeeMore={() => navigate("/expense")}
+          />
+
+          <Last30DaysExpenses
+            transactions={dashboardData?.last30DaysExpenses?.transactions || []}
+          />
+
+          <RecentIncomeWithChart
+            data={(dashboardData?.last60DaysIncomes?.transactions || []).slice(
+              0,
+              4,
+            )}
+            totalIncome={dashboardData?.totalIncome || 0}
+          />
+
+          <RecentIncome
+            transactions={dashboardData?.last60DaysIncomes?.transactions || []}
+            onSeeMore={() => navigate("/income")}
+          />
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Home;
