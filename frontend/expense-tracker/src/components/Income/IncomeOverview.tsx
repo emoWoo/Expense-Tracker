51374c9798e4bf0336caf45bf2f4b9307c420af5
吴伊玲 <woo12310@163.com>
2026-04-06 15:29:09 +0800

    feat(IncomeOverview): 更新收入概览组件，调整文本为中文并注释掉图表部分

--- frontend/expense-tracker/src/components/Income/IncomeOverview.tsx
+++ frontend/expense-tracker/src/components/Income/IncomeOverview.tsx
@@ -4,32 +4,38 @@ import { prepareIncomeBarChartData } from "../../utils/helper";
 import { LuPlus } from "react-icons/lu";
 import CustomBarChart from "../Charts/CustomBarChart";
 
+type GroupedIncomeItem = {
+  date: string;
+  income: Transaction[];
+};
 interface IncomeOverviewProps {
-  transactions: Transaction[];
+  transactions: GroupedIncomeItem[];
   onAddIncome?: () => void;
 }
 
 const IncomeOverview = ({ transactions, onAddIncome }: IncomeOverviewProps) => {
-  const chartData = useMemo(
-    () => prepareIncomeBarChartData(transactions),
-    [transactions],
-  );
+  // const chartData = useMemo(
+  //   () => prepareIncomeBarChartData(transactions),
+  //   [transactions],
+  // );
 
   return (
     <div className="card">
       <div className="flex flex-wrap items-center justify-between">
         <div className="">
-          <h5 className="text-lg">income overview</h5>
-          <p className="text-xs text-gray-400 mt-0.5">abcabc</p>
+          <h5 className="text-lg">收入概览</h5>
+          <p className="text-xs text-gray-400 mt-0.5">
+            实时记录你的收入，并提醒你多挣点钱
+          </p>
         </div>
 
         <button className="add-btn" onClick={onAddIncome}>
           <LuPlus className="text-lg" />
-          Add Income
+          增加收入
         </button>
       </div>
       <div className="mt-10">
-        <CustomBarChart data={chartData} type="income" />
+        {/* <CustomBarChart data={chartData} type="income" /> */}
       </div>
     </div>
   );
